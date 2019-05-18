9e1a244815cc330b8e0dd10511387810557b89d8
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2019-05-18 15:48:03 +0200

    Import initial version

---
+++ random_tweets.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import account_secrets
+import json
+import secrets
+import time  # sleep
+import twython
+
+
+#KEYWORDS = ['uutiset', 'ニュース', 'hírek', 'Νέα', 'невс', 'חדשות', 'খবর', 'أخبار', 'ข่าว', 'समाचार', 'мэдээ', 'خبریں', 'naidheachdan']
+KEYWORDS = ['uutiset', 'ニュース', 'חדשות', 'খবর', 'أخبار']
+
+
+class StreamerIn(twython.TwythonStreamer):
+    def __init__(self, pk, ps, ct, cts):
+        super.__init__(pk, ps, ct, cts)
+        self.twitter = twython.Twython(pk, access_token=ct)
+
+    def do_retweet(self, tweet):
+        pass
+
+    def on_success(self, tweet_data):
+        if 'text' not in tweet_data:
+            return
+        if 'scopes' in tweet_data and 'followers' in tweet_data['scopes'] and not tweet_data['scopes']['followers']:
+            # Ad
+            return
+        with open('tweets/{}_{}.json'.format(secrets.token_hex(8), tweet_data.get('id')), 'w') as fp:
+            json.dump(tweet_data, fp, sort_keys=True, indent=1)
+        # TODO: Retweet it.
+        # retained = dict()
+        # for key in ['id', 'lang', 'text']:
+        #     if key in tweet_data:
+        #         retained[key] = tweet_data[key]
+        # print(retained)
+
+    def on_error(self, status_code, data):
+        print(status_code, data)
+
+
+def run_firehose():
+    print('Waiting ...')
+    time.sleep(2)
+    print('Starting up')
+    stream = StreamerIn(account_secrets.API_KEY, account_secrets.API_SECRET, account_secrets.ACCESS_TOKEN,
+                        account_secrets.ACCESS_TOKEN_SECRET)
+    print('Begin filtering ...')
+    stream.statuses.filter(track=KEYWORDS)
+    print('Done filtering ...')
+
+
+if __name__ == '__main__':
+    run_firehose()
